e74a2fa7f5fa493ec449198b24dd6233e158e42d
Percy-Potter2008 <75735895+Percy-Potter2008@users.noreply.github.com>
2021-05-27 19:15:47 +0530

    Create mainKidstagram.js

---
+++ mainKidstagram.js
@@ -0,0 +1,12 @@
+ // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBY-76K3LBT3LEmOUeKkzqou1-4rxB5X2c",
+    authDomain: "kidstagram-3a92c.firebaseapp.com",
+    projectId: "kidstagram-3a92c",
+    storageBucket: "kidstagram-3a92c.appspot.com",
+    messagingSenderId: "202592612252",
+    appId: "1:202592612252:web:0f7ae177ff594059726259"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
