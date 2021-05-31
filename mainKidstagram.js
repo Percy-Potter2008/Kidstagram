bed53096fb6147b2b68059e76947e86aaa2c9449
Percy-Potter2008 <75735895+Percy-Potter2008@users.noreply.github.com>
2021-05-31 19:20:26 +0530

    Update mainKidstagram.js

--- mainKidstagram.js
+++ mainKidstagram.js
@@ -10,8 +10,9 @@
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   
-function ADD-USER() {
-  User_name= document.getElementById("user_name").value;
- localStorage.setItem("User_name", User_name);
- window.location="Kidstagram.html";
+function ADDUSER() {
+  user_name= document.getElementById("user_name").value;
+ firebase.database().ref("/").child(user_name).update({
+  purpose : "adding user"
+ });
 }
