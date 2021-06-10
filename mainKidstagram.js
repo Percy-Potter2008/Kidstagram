a1913390160214556cb20da33bbec1d2f27a4e1d
Percy-Potter2008 <75735895+Percy-Potter2008@users.noreply.github.com>
2021-06-10 19:51:45 +0530

    Update mainKidstagram.js

--- mainKidstagram.js
+++ mainKidstagram.js
@@ -18,4 +18,4 @@ function adduser() {
  });
  localStorage.setItem("username", username);
  window.location="index2.html";
-}
+function addUser() { user_name = document.getElementById("user_name").value; localStorage.setItem("user_name", user_name); window.location = "kwitter_room.html"; 
